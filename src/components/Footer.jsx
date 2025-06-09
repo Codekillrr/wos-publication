ae052c1fdaef9110b0e0cedf968fe7a48cfe1279
Abdur Rehman <abdurrehman@Abdurs-MacBook-Air.local>
2025-06-09 12:39:57 +0500

    fixed logo sizing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,20 +22,29 @@ const Footer = () => {
       </nav>
       <nav className="text-white">
         <h6 className="footer-title opacity-100 text-yellow-200">Company</h6>
+        <a className="link link-hover">Home</a>
         <a className="link link-hover">About us</a>
         <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
       </nav>
       <nav className="text-white">
         <h6 className="footer-title opacity-100 text-yellow-200">Lets Talk</h6>
-        <a className="link link-hover">info@wospublication.com</a>
-        <a className="link link-hover">+44 7724 695183</a>
+        <a href="tel:+447724695183" className="link link-hover">
+          +44 7724 695183
+        </a>
+        <a href="mailto:contact@wospublication.com" className="link link-hover">
+          contact@wospublication.com
+        </a>
+        <a href="mailto:info@wospublication.com" className="link link-hover">
+          info@wospublication.com
+        </a>
+        <a href="mailto:help@wospublication.com" className="link link-hover">
+          help@wospublication.com
+        </a>
       </nav>
       <nav className="text-white">
         <h6 className="footer-title text-yellow-300">Social</h6>
         <div className="grid grid-flow-col gap-4">
-          <a>
+          {/* <a>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -46,7 +55,7 @@ const Footer = () => {
             >
               <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
             </svg>
-          </a>
+          </a> */}
           <a
             href="https://wa.me/447724695183?text=Hi%20there%2C%20I%20have%20a%20question!"
             target="_blank"
